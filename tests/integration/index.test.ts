dc42c554e9feaf70c906681d0982069d866cde56
Eka Hasany <eka.hasany@avimedical.com>
2022-03-16 14:52:00 +0100

    feat: integration tests

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -57,4 +57,40 @@ describe("'createRootCaVerifier' must work correctly", () => {
     expect(await verifierBasedOnFile(request)).toEqual(expectedResult);
     expect(await verifierBasedOnArray(request)).toEqual(expectedResult);
   });
+
+  let noHostValidationVerifierBasedOnFile: CertificateVerifier;
+  let noHostValidationVerifierBasedOnArray: CertificateVerifier;
+
+  test("should create no host validation verifier with given path to '*.pem' file without an error", () => {
+    const pathname = path.resolve(__dirname, "./testSamples/cacert.pem");
+    expect(() => {
+      noHostValidationVerifierBasedOnFile = createRootCaVerifier(pathname, false);
+    }).not.toThrow();
+  });
+
+  test("should create no host validation verifier with given array of pem without an error", () => {
+    expect(() => {
+      noHostValidationVerifierBasedOnArray = createRootCaVerifier(rootCaList, false);
+    }).not.toThrow();
+  });
+
+  // The wrongHostRequest should return VALID (0) since no host validation is done, others shouldn't be effected
+  test.each`
+    request                  | expectedResult
+    ${redditCom}             | ${VerificationResult.VALID}
+    ${firebaseCom}           | ${VerificationResult.VALID}
+    ${exampleOrg}            | ${VerificationResult.VALID}
+    ${pgGateway}             | ${VerificationResult.VALID}
+    ${domainWithExpiredCert} | ${VerificationResult.INVALID}
+    ${untrustedRoot}         | ${VerificationResult.INVALID}
+    ${selfSigned}            | ${VerificationResult.INVALID}
+    ${wrongHostRequest}      | ${VerificationResult.VALID}
+    ${youtubeComRequest}     | ${VerificationResult.VALID}
+  `(
+    "no host validation verifier should return $expectedResult ($request.hostname)",
+    async ({ request, expectedResult }) => {
+      expect(await noHostValidationVerifierBasedOnFile(request)).toEqual(expectedResult);
+      expect(await noHostValidationVerifierBasedOnArray(request)).toEqual(expectedResult);
+    },
+  );
 });
